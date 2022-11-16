d68c789641f05f29053401fa933bdd1d85778620
Brian Nganga <briannganga70@gmail.com>
2022-11-16 16:08:15 +0300

    urls

--- firestore/urls.py
+++ firestore/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.view_name, name="view_name"),
+    path('', views.fire, name="fire"),
 ]
