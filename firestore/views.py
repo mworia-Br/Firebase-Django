c5e7d3cc0f626608c349da014a433a75b39bd5df
Brian Nganga <briannganga70@gmail.com>
2022-11-16 15:40:49 +0300

    firebase config

--- firestore/views.py
+++ firestore/views.py
@@ -7,7 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #firebase config
+config = {
+    "apiKey": "AIzaSyDYU7Ry7YnlfvBl4LKIork_AIcRTDZ3gGE",
+    "authDomain": "jls-react.firebaseapp.com",
+    "databaseURL": "https://jls-react-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "jls-react",
+    "storageBucket": "jls-react.appspot.com",
+    "messagingSenderId": "599654946369",
+    "appId": "1:599654946369:web:800da4c47682174c34d8c0",
+    "databaseURL": "",
+}
 
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
 
 def view_name(request):
     return render(request, 'fire.html', {})
