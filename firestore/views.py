524f12f4bb683ba3582cf6056f92c11077e96949
Brian Nganga <briannganga70@gmail.com>
2022-11-16 15:02:15 +0300

    Imports

--- firestore/views.py
+++ firestore/views.py
@@ -1,4 +1,13 @@
+import os
+import pyrebase
 from django.shortcuts import render
+from dotenv import load_dotenv
+
+# the load_dotenv function gets the environment variables defined in .env file
+load_dotenv()
+
+#firebase config
+
 
 def view_name(request):
     return render(request, 'fire.html', {})
